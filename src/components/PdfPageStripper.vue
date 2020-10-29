43aa8df7c3b3d0589d1f29588882658b1e9fbea7
stefnotch <brandmairstefan@gmail.com>
2020-10-29 21:16:48 +0100

    Add error checking

--- src/components/PdfPageStripper.vue
+++ src/components/PdfPageStripper.vue
@@ -35,8 +35,10 @@ function usePdfLib() {
   async function stripPdf(bytes: ArrayBuffer) {
     const pdfFile = await PDFDocument.load(bytes);
     const pageLabels = pdfFile.catalog.get(PDFName.of("PageLabels"));
+    if (!pageLabels) return;
     const pageNumbers = (pageLabels as any).get(PDFName.of("Nums"))
       .array as any[];
+    if (!pageNumbers) return;
 
     const pagesToKeep = new Set<number>();
     for (let i = 0; i < pageNumbers.length; i++) {
@@ -47,13 +49,16 @@ function usePdfLib() {
       }
     }
 
+    let deletedAnyPage = false;
     const pageCount = pdfFile.getPageCount();
     // NOTE: pageCount - 2, because we always want to keep the last page
     for (let i = pageCount - 2; i >= 0; i--) {
       if (!pagesToKeep.has(i)) {
         pdfFile.removePage(i);
+        deletedAnyPage = true;
       }
     }
+    if (!deletedAnyPage) return;
     return pdfFile;
   }
 
@@ -79,11 +84,15 @@ export default defineComponent({
 
       for (let i = 0; i < files.length; i++) {
         const file = files.item(i);
-        const result = new Blob(
-          [await (await pdfLib.stripPdf(await file.arrayBuffer())).save()],
-          { type: "application/pdf" }
-        );
-        pdfFiles.value[i].result = window.URL.createObjectURL(result);
+        const strippedPdf = await pdfLib.stripPdf(await file.arrayBuffer());
+        if (strippedPdf) {
+          const result = new Blob([await strippedPdf.save()], {
+            type: "application/pdf",
+          });
+          pdfFiles.value[i].result = window.URL.createObjectURL(result);
+        } else {
+          pdfFiles.value[i].name += " - found nothing to strip";
+        }
       }
     }
 

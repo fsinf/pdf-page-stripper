f5876f80d7b8f4ff5d4fd208da41786826dfa574
Johannes Zottele <jozott@MacBook-Air-von-Johannes.local>
2021-02-11 18:36:16 +0100

    zip feature added

--- src/components/PdfPageStripper.vue
+++ src/components/PdfPageStripper.vue
@@ -2,7 +2,7 @@
   <form action="#">
     <h3>PDF Stripper</h3>
     <div class="file-field input-field">
-      <div class="btn">
+      <div class="btn card">
         <span>PDFs</span>
         <input
           type="file"
@@ -16,6 +16,8 @@
 
   <div>
     <h3>Stripped PDFs</h3>
+    <p v-if="zipping.ready == 0"> ZIP in process ... </p>
+    <a v-if="zipping.ready == 1" download="stripped-pdf" :href="zipping.result"> Download as ZIP </a>
     <ul>
       <li v-for="item in pdfFiles" :key="item.id">
         {{ item.name }}
@@ -29,29 +31,39 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import { PDFDocument, PDFName } from "pdf-lib";
+import { PDFArray, PDFDict, PDFDocument, PDFName, PDFNumber, PDFObject } from "pdf-lib";
+import JSZip from "jszip";
+
+interface PDFResult { id: string; name: string; result?: string };
 
 function usePdfLib() {
+  /**
+   * It finds the index of the last page of each defined page label of the PDF document. The only the found pages are returned.
+   * 
+   * More information about page labels:
+   * https://www.adobe.com/content/dam/acom/en/devnet/pdf/pdfs/pdf_reference_archives/PDFReference.pdf#page=501&zoom=100,118,506
+   * @returns PDFDocument with last pages of each label range
+   */
   async function stripPdf(bytes: ArrayBuffer) {
     const pdfFile = await PDFDocument.load(bytes);
-    const pageLabels = pdfFile.catalog.get(PDFName.of("PageLabels"));
+    const pageLabels = <PDFDict> pdfFile.catalog.get(PDFName.of("PageLabels"));
     if (!pageLabels) return;
-    const pageNumbers = (pageLabels as any)?.get?.(PDFName.of("Nums"))
-      ?.array as any[];
+    const pageNumbers = <PDFObject[]>(<PDFArray> pageLabels.get(PDFName.of("Nums"))).asArray();
     if (!pageNumbers) return;
 
     const pagesToKeep = new Set<number>();
-    for (let i = 0; i < pageNumbers.length; i++) {
+    for (let i = 1; i < pageNumbers.length; i++) {
       const element = pageNumbers[i];
-      if (element.numberValue !== undefined) {
+      if(element instanceof PDFNumber) {
         // Page number - 1 ==> index
-        pagesToKeep.add(element.numberValue - 1);
+        // each page number == first index of label range ... page number - 1 == last index of previous label range
+        pagesToKeep.add(element.asNumber() - 1);
       }
     }
 
     let deletedAnyPage = false;
     const pageCount = pdfFile.getPageCount();
-    // NOTE: pageCount - 2, because we always want to keep the last page
+    // NOTE: pageCount - 2, because we always want to keep the last page (since it is not included in the set)
     for (let i = pageCount - 2; i >= 0; i--) {
       if (!pagesToKeep.has(i)) {
         pdfFile.removePage(i);
@@ -62,13 +74,34 @@ function usePdfLib() {
     return pdfFile;
   }
 
-  return { stripPdf };
+  /**
+   * Converts all striped PDF to one zip file.
+   * 
+   * @returns URL to blob(with zip file) or undefined if less than 2 pdf got striped
+   */
+  async function zipFiles(files: PDFResult[]) {
+    const zip = new JSZip();
+
+    for(let i = 0; i < files.length; i++){
+      if(files[i].result === undefined)
+        continue;
+      const blob = await fetch(files[i].result).then(r => r.blob());
+      zip.file(files[i].name, blob);
+    }
+
+    const resZip = await zip.generateAsync<"blob">({type: "blob", compression: "DEFLATE"});
+    return window.URL.createObjectURL(resZip);
+  }
+
+  return { stripPdf, zipFiles };
 }
 
 export default defineComponent({
   setup() {
     const pdfLib = usePdfLib();
-    const pdfFiles = ref<{ id: string; name: string; result?: string }[]>([]);
+    const pdfFiles = ref<PDFResult[]>([]);
+
+    let zipping = ref<{ ready: number, result?: string}>({ready: -1});
 
     async function onFilesSelected(event: InputEvent) {
       pdfFiles.value.length = 0;
@@ -76,29 +109,35 @@ export default defineComponent({
 
       for (let i = 0; i < files.length; i++) {
         const file = files.item(i);
+
         pdfFiles.value.push({
           id: "" + i,
           name: `stripped-${file.name}`,
         });
-      }
-
-      for (let i = 0; i < files.length; i++) {
-        const file = files.item(i);
+        
         const strippedPdf = await pdfLib.stripPdf(await file.arrayBuffer());
         if (strippedPdf) {
           const result = new Blob([await strippedPdf.save()], {
             type: "application/pdf",
           });
           pdfFiles.value[i].result = window.URL.createObjectURL(result);
+
+          zipping.value.ready = 0;
         } else {
           pdfFiles.value[i].name += " - found nothing to strip";
         }
+      }
+
+      if(zipping.value.ready === 0) {
+        zipping.value.result = await pdfLib.zipFiles(pdfFiles.value);
+        zipping.value.ready = 1;
       }
     }
 
     return {
       onFilesSelected,
       pdfFiles,
+      zipping,
     };
   },
 });

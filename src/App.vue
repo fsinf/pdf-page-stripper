d53a870d01f4444a446002c2cf8d0712ea943a00
stefnotch <brandmairstefan@gmail.com>
2021-02-12 17:16:24 +0100

    Bump dependencies

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
 </template>
 
 <script>
+import { defineComponent } from "vue";
 import PdfPageStripper from "./components/PdfPageStripper.vue";
 
-export default {
+export default defineComponent({
   name: "App",
   components: {
     PdfPageStripper,
   },
-};
+});
 </script>
